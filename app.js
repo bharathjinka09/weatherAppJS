cd5fbcd8a83872a6b769fba2056ff2be71c668b7
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-28 11:29:04 +0530

    Completed project

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Tutorial by http://youtube.com/CodeExplained
-// api key : 82005d27a116c2880c8f0fcb866998a0
+// api key : 9b2ff67bcf783bf351ac8bb85a666f48
 
 // SELECT ELEMENTS
 const iconElement = document.querySelector(".weather-icon");
@@ -18,7 +18,7 @@ weather.temperature = {
 // APP CONSTS AND VARS
 const KELVIN = 273;
 // API KEY
-const key = "82005d27a116c2880c8f0fcb866998a0";
+const key = "9b2ff67bcf783bf351ac8bb85a666f48";
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
